dd2054f4d6a6a9ea975bb9ce059bb2194a80e8ef
Arailson Santos <arailson.vieira@gmail.com>
2022-01-03 19:33:49 -0300

    Listando os dados da pagina

--- js/index.js
+++ js/index.js
@@ -1,14 +1,49 @@
-function validar (){
-    var nomeMercadoria = document.getElementById("nomeMercadoria");
-    var valor = document.getElementById("valor") 
+var extrato = [
+    {
+        descricaoSinal: '+',
+        descricaoTexto: 'Lorem ipsum dolor sit amet consectetur',
+        valorTransacao: 'R$ 12.999,99'
+    },
+    {
+        descricaoSinal: '-',
+        descricaoTexto: 'Quis nostrud exercitation',
+        valorTransacao: 'R$ 99,99'
+    },
+    {
+        descricaoSinal: '+',
+        descricaoTexto: 'Lorem ipsum',
+        valorTransacao: 'R$ 9,99'
+    }
+]
 
-    if (nomeMercadoria.value == ""){
+for (infoExtrato in extrato) {
+    document.querySelector("div.extrato-transacao-item").innerHTML +=`<div class="descricao-valor">
+       <div class="descricao-transacao">
+           <p class="descricao-sinal"> ${extrato[infoExtrato].descricaoSinal} </p>
+           <p class="descricao-texto">${extrato[infoExtrato].descricaoTexto}</p>
+       </div>
+       <p class="valor-transacao">${extrato[infoExtrato].valorTransacao}</p>
+    </div>
+    <div class="linha-horizontal"></div`
+}
+
+
+
+
+
+
+function validar() {
+    var nomeMercadoria = document.getElementById("nomeMercadoria");
+    var valor = document.getElementById("valor")
+
+    if (nomeMercadoria.value == "") {
         alert('Nome não informado')
         return;
     }
 
-    if (valor.value ==""){
+    if (valor.value == "") {
         alert('Valor não informado');
         return;
     }
+
 }
